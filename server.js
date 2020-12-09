ee0f5c3634f77968a86a3fb73ff83bc5a7e95b2b
Christian Sinai <christian_sinai@me.com>
2020-12-08 20:38:09 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(
+     process.env.MONGODB_URI || 'mongodb://localhost/Services',
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     }
+);
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!")
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
