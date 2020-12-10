d14d97ce25ff5d5e73f293f5897a5eb2d281bb70
pablito <pabloivanjuarez@gmail.com>
2020-12-09 21:13:02 -0800

    rm redundant code

--- server.js
+++ server.js
@@ -1,29 +1,4 @@
 const express = require("express");
-
-
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-
-// Start the API server
-app.listen(PORT, function () {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
-=======
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
@@ -31,13 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
-     process.env.MONGODB_URI || 'mongodb://localhost/Services',
-     {
-       useNewUrlParser: true,
-       useUnifiedTopology: true,
-       useCreateIndex: true,
-       useFindAndModify: false
-     }
+  process.env.MONGODB_URI || 'mongodb://localhost/Services',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
 );
 
 mongoose.connection.on("connected", () => {
