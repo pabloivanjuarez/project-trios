91ba32ee0bc40d3107e5fc8e5ccb68aad4493dd6
pablito <pabloivanjuarez@gmail.com>
2020-12-10 19:47:31 -0800

    Jumbotron code added from first_html

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-function MiddleBlock() {
+function Jumbotron() {
   return (
-    <div>
-
+    <div class="container-fluid">
+      <div class="row">
+        <div class="jumbotron">
+          <div class="jtron-image">
+            <div class="jumbotext">
+              <h1 class="jumbotext display-4 fw-normal">The Best Tech Help</h1>
+              <p class="jumbotext">Right in your Neighborhood</p>
+              <a class="btn btn-outline-primary" href="#">Coming soon</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default MiddleBlock
+export default Jumbotron
