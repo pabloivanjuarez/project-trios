e58c190db272075791058be3e2d2756aab3cc4a3
Hayden Rust <haydenrust1@gmail.com>
2020-12-10 20:28:04 -0800

    skeleton server files created

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+// IF: API Routes
+router.use("/api", apiRoutes);
+
+//ELSE: React App
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
