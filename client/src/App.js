ac82d820994dcac52e4dbe03c3950707ef48d767
pablito <pabloivanjuarez@gmail.com>
2020-12-12 13:13:48 -0800

    rm unused starter code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Map from './components/Map/index';
 import Header from "./components/Header/index"
+import Jumbotron from "./components/Jumbotron/index";
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Jumbotron />
       <Map />
     </div>
   );
