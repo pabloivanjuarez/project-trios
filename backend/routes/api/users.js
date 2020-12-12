74267aad10147609f87ad04a5f6ed8366f0838ff
pablito <pabloivanjuarez@gmail.com>
2020-12-12 13:56:49 -0800

    sudo code added on line 3

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
+//userController vs servicesController
+
 
 // Matches with "/api/users"
 router.route("/")
