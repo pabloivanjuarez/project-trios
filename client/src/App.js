540ebdc51c73abb90b5ad77ace7bdf9d7ce1c86a
M. Adamson <MX@Matthews-MBP.lan>
2020-12-12 13:12:02 -0800

    importing footer and jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map/index';
 import Header from "./components/Header/index"
+import Footer from "./components/Footer/index"
+import Jumbotron from "./components/Jumbotron/index"
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Jumbotron />
       <Map />
+      <Footer />
     </div>
   );
 }
