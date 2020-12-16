92838b203eaaaa797c41faa081846e71edaad94d
Hayden Rust <haydenrust1@gmail.com>
2020-12-15 21:27:12 -0800

    added React routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
-import Map from './components/Map/index';
-import Header from "./components/Header/index"
-import Footer from "./components/Footer/index"
-import Jumbotron from "./components/Jumbotron/index"
-import Services from "./components/Services/index"
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Header from "./components/Header/index";
+import Footer from "./components/Footer/index";
+import Home from "./pages/Home";
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Jumbotron />
-      <Services />
-      <Map />
-      <Footer />
-    </div>
+    <Router>
+        <Header />
+        {/* Routes */}
+        <Switch>
+
+          {/* Home Page */}
+          <Route path={["/", "/home"]}>
+            <Home />
+          </Route>
+
+          {/* Login Page */}
+          <Route path="/login">
+            <Login />
+          </Route>
+          
+        </Switch>
+        <Footer />
+    </Router>
   );
 }
 
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Nerds Next Door</a>
+          <a className="navbar-brand" href="/">Nerds Next Door</a>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -16,13 +16,13 @@ function Header() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active" aria-current="page" href="/home">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Sign Up</a>
+                <a className="nav-link" href="/login">Sign Up</a>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                <a className="nav-link dropdown-toggle" href="/services" id="navbarDropdown" role="button"
                   data-bs-toggle="dropdown" aria-expanded="false">
                   Services
                         </a>
