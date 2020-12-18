6f124b98ec74fed548c0107b84ea9e954eca1972
pablito <pabloivanjuarez@gmail.com>
2020-12-17 18:55:28 -0800

    Header code && styles

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './Header.module.css'
 
 function Header() {
   return (
@@ -7,13 +8,13 @@ function Header() {
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Nerds Next Door</a>
 
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+          <button className="navbar-toggler" className={Header['burger']} type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
             aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse" className={Header['navbar']} id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
@@ -36,15 +37,15 @@ function Header() {
                   <li><a className="dropdown-item" href="#">Other</a></li>
                 </ul>
               </li>
-        
-                </ul>
+
+            </ul>
             {/* <form className="d-flex">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button className="btn btn-outline-success" type="submit">Search</button>
                 </form> */}
-            </div>
           </div>
-    </nav>
+        </div>
+      </nav>
     </div>
   )
 }
