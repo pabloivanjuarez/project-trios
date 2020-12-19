b17aecc78865ce3df06d163ae4cea4c1427f157a
M. Adamson <MX@Matthews-MBP.lan>
2020-12-19 09:22:46 -0800

    fixing homepage

--- client/src/App.js
+++ client/src/App.js
@@ -9,27 +9,29 @@ import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
 import UpdateProfile from "./components/UpdateProfile";
 import Header from "./components/Header/index";
+import Home from "./pages/Home"
 
 function App() {
   return (
     <Router>
     <Header />
-    <Container
+    {/* <Container
       className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
+      style={{ minHeight: "100vh" }} */}
     >
       <div>
           <AuthProvider>
             <Switch>
               <PrivateRoute path="/dashboard" component={Dashboard} />
               <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route exact path="/" component={Login} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
               <Route path="/signup" component={Signup} />
               <Route path="/forgot-password" component={ForgotPassword} />
             </Switch>
           </AuthProvider>
       </div>
-    </Container>
+    {/* </Container> */}
     </Router>
   )
 };
